3cf1ecdc717f5addde9153a48de33d71cb695102
UncleLeo037 <leo.muller@gmail.com>
2026-02-03 10:58:55 +0000

    adjusted tornado spell

--- spells/Tornado.cs
+++ spells/Tornado.cs
@@ -6,19 +6,23 @@ public partial class Tornado : Node3D
 {
 	private double lifetime = 3;
 	Area3D area;
+	Node3D center;
+	Node3D pull;
 	public override void _Ready()
 	{
 		area = GetNode<Area3D>("Area3D");
+		center = GetNode<Node3D>("Center");
+		pull = center.GetNode<Node3D>("Pull");
 	}
 
 	public override void _Process(double delta)
 	{
 		this.Rotation = Vector3.Zero;
 		//defines tornado center pull point, need to make rotating
-		Vector3 pull = new Vector3(0, 5, 0);
+		center.RotateY((float)(delta * 10.0));
 		foreach (Player player in area.GetOverlappingBodies())
 		{
-			player.Velocity = ((this.GlobalTransform.Origin + pull) - player.GlobalTransform.Origin).Normalized() * 20.0f;
+			player.Velocity = (pull.GlobalTransform.Origin - player.GlobalTransform.Origin).Normalized() * 15.0f;
 		}
 
 		lifetime -= delta;

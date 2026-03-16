64532c6ca207e265388fe30ae8ab6e7c02f155b2
UncleLeo037 <leo.muller@gmail.com>
2026-03-16 11:13:21 +0000

    Changed game logo

--- gun/Gun.cs
+++ gun/Gun.cs
@@ -5,15 +5,15 @@ public partial class Gun : Equipment
 {
 	public override void Equip()
 	{
-        //will need to call un-equip for currently equiped gun if there is one
-        //will need to set self as active gun for player
-        //will need to show gun model
-        throw new NotImplementedException();
+		//will need to call un-equip for currently equiped gun if there is one
+		//will need to set self as active gun for player
+		//will need to show gun model
+		throw new NotImplementedException();
 	}
 
 	public override void Unequip()
 	{
-        //will need to hide gun model
-        throw new NotImplementedException();
+		//will need to hide gun model
+		throw new NotImplementedException();
 	}
 }

a9762d53fb4da79945ab9b82aeff25b69f9ac90d
UncleLeo037 <leo.muller@gmail.com>
2026-05-11 19:54:20 +0100

    gun changes and export settings

--- gun/Gun.cs
+++ gun/Gun.cs
@@ -5,44 +5,47 @@ public partial class Gun : Node3D
 {
 	private PackedScene gunModel;
 	private RayCast3D _rayCast;
-	private AnimationPlayer _animPlay;
+	private AnimationPlayer _anime;
 	private GpuParticles3D _flash;
 	public string equipedSpell = string.Empty;
 
 	public override void _Ready()
 	{
 		_rayCast = GetNode<RayCast3D>("RayCast3D");
-		_animPlay = GetNode<AnimationPlayer>("AnimationPlayer");
+		_anime = GetNode<AnimationPlayer>("AnimationPlayer");
 		_flash = GetNode<Node3D>("Node3D").GetNode<GpuParticles3D>("GPUParticles3D");
 	}
 
 	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true)]
 	private void ShootAnim()
 	{
-		_animPlay.Stop();
-		_animPlay.Play("shoot");
+		_anime.Stop();
+		_anime.Play("shoot");
 		_flash.Restart();
 		_flash.Emitting = true;
 	}
 
 	public void Shoot()
 	{
-		//add RPC shoot animation call. DO NOT move RPC from spell spawn to here! could cause spell sync issues
-		this.ShootAnim();
-		var target = _rayCast.GetCollider();
-		if(target != null)
+		if (_anime.CurrentAnimation != "shoot")
 		{
-			//should just shoot instead
-			if (equipedSpell != string.Empty)
+			//add RPC shoot animation call. DO NOT move RPC from spell spawn to here! could cause spell sync issues
+			this.ShootAnim();
+			var target = _rayCast.GetCollider();
+			if (target != null)
 			{
-				//spells will be called in different ways here in future and equipedSpell will be an object
-				Vector3 point = _rayCast.GetCollisionPoint();
-				SpellSpawner.CastSpell(this.Name, equipedSpell, point, new Vector3(this.GlobalRotation.X, this.GlobalRotation.Y, 0));
-				//equipedSpell = string.Empty;
-			}
-			if (target is Player player)
-			{
-				player.Rpc("Damage");
+				//should just shoot instead
+				if (!string.IsNullOrEmpty(equipedSpell))
+				{
+					//spells will be called in different ways here in future and equipedSpell will be an object
+					Vector3 point = _rayCast.GetCollisionPoint();
+					SpellSpawner.CastSpell(this.Name, equipedSpell, point, new Vector3(this.GlobalRotation.X, this.GlobalRotation.Y, 0));
+					equipedSpell = string.Empty;
+				}
+				else if (target is Player player)
+				{
+					player.Rpc("Damage");
+				}
 			}
 		}
 	}

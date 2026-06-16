33b523dd959858af856cfd804e09dde3f58bb15e
UncleLeo037 <leo.muller@gmail.com>
2026-06-16 22:24:44 +0100

    enemy refinements
    enemy looks in direction of movement

--- enemies/ghost/Ghost.cs
+++ enemies/ghost/Ghost.cs
@@ -5,8 +5,11 @@ public partial class Ghost : CharacterBody3D
 {
 	private NavigationAgent3D navAgent;
 
-	private float speed = 2.0f;
-    private Player target;
+
+
+	private float speed = 3.0f;
+	private Player target;
+	private float memory = 10.0f;
 
 	public override void _Ready()
 	{
@@ -14,29 +17,44 @@ public partial class Ghost : CharacterBody3D
 		navAgent.PathDesiredDistance = 0.5f;
 		navAgent.TargetDesiredDistance = 0.5f;
 
-		Area3D area = GetNode<Area3D>("Area3D");
-		area.BodyEntered += Detect;
+		Area3D vision = GetNode<Area3D>("Area3D");
+		vision.BodyEntered += Detect;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
 		if (!IsMultiplayerAuthority()) return;
-		base._PhysicsProcess(delta);
-		if (target != null)
+		if (!IsOnFloor())
 		{
-			navAgent.TargetPosition = target.GlobalTransform.Origin;
+			Velocity += GetGravity() * (float)delta;
 		}
-        if (navAgent.IsNavigationFinished())
-        {
-            return;
-        }
+		else
+		{
+			if (target != null)
+			{
+				navAgent.TargetPosition = target.GlobalTransform.Origin;
+				memory -= 1 * (float)delta;
+				if (memory < 0)
+				{
+					target = null;
+					memory = 5.0f;
+				}
+			}
+			if (navAgent.IsNavigationFinished())
+			{
+				return;
+			}
 
-        Vector3 currentAgentPosition = GlobalTransform.Origin;
-        Vector3 nextPathPosition = navAgent.GetNextPathPosition();
+			Vector3 currentAgentPosition = GlobalTransform.Origin;
+			Vector3 nextPathPosition = navAgent.GetNextPathPosition();
 
-        Velocity = currentAgentPosition.DirectionTo(nextPathPosition) * speed;
-        MoveAndSlide();
+			Velocity = currentAgentPosition.DirectionTo(nextPathPosition) * speed;
+
+			//forgets player direction when player is null but can add look about code then
+			var angle = Rotation.AngleTo(new Vector3(0, Velocity.X, Velocity.Z));
+			Rotation = Rotation.Lerp(new Vector3(0, angle, 0), (float)delta * 5);
+		}
+		MoveAndSlide();
 	}
 
 	public void Detect(Node3D node)

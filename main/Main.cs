872dbb59497b96b94c3b87fcedcbd9dd982dff40
UncleLeo037 <leo.muller@gmail.com>
2026-06-29 21:38:05 +0100

    Multiplayer Improvements
    Moved enet to Main.cs and add map spawner

--- main/Main.cs
+++ main/Main.cs
@@ -3,19 +3,98 @@ using System;
 
 public partial class Main : Node3D
 {
+	[Export] public bool IsEnet = false;
 	private Control Menu;
 	private Button BtnHost;
 	private Button BtnJoin;
 
+	private Control Pause;
+	private Button BtnExit;
 
-	private Button BtnIP;
+	const int PORT = 9999;
+	public ENetMultiplayerPeer EnetPeer;
+
+	public MultiplayerSpawner Spawner;
+	public MultiplayerSpawner MapSpawner;
 
 	public override void _Ready()
 	{
-		GetNode("MultiplayerSpawner").Call("manual_ready");
+		Spawner = GetNode<MultiplayerSpawner>("MultiplayerSpawner");
+		Spawner.AddSpawnableScene("res://player/Player.tscn");
+		MapSpawner = GetNode<MultiplayerSpawner>("WorldSpawner");
+		MapSpawner.AddSpawnableScene("res://maps/hub/Hub.tscn");
+
+		Menu = Spawner.GetNode<Control>("Menu");
+		BtnHost = Menu.GetNode<Button>("Host");
+		BtnJoin = Menu.GetNode<Button>("Join");
+		Pause = Spawner.GetNode<Control>("Pause");
+		BtnExit = Pause.GetNode<Button>("Exit");
+
+		if (IsEnet)
+		{
+			BtnHost.Pressed += HostEnet;
+			BtnJoin.Pressed += JoinEnet;
+			BtnJoin.Disabled = false;
+			BtnExit.Pressed += OnExit;
+			EnetPeer = new ENetMultiplayerPeer();
+		}
+		else
+		{
+			Spawner.Call("manual_ready");
+		}
+
+		GetTree().Paused = false;
 	}
 
-	public override void _Process(double delta)
+	public override void _UnhandledInput(InputEvent @event)
 	{
+		if (@event.IsActionPressed("escape") && !Menu.IsVisibleInTree())
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+			Pause.Show();
+		}
+
+		//should move this to resume button
+		if (@event is InputEventMouseButton && !Menu.IsVisibleInTree() && Input.MouseMode != Input.MouseModeEnum.ConfinedHidden)
+		{
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+			Pause.Hide();
+		}
+	}
+
+
+	public void HostEnet()
+	{
+		Menu.Hide();
+		EnetPeer.CreateServer(PORT);
+		Multiplayer.MultiplayerPeer = EnetPeer;
+		Multiplayer.PeerConnected += AddPlayer;
+		Multiplayer.PeerDisconnected += RemovePlayer;
+
+		AddChild(GD.Load<PackedScene>("res://maps/hub/Hub.tscn").Instantiate());
+		Spawner.Call("_add_player", Multiplayer.GetUniqueId());
+	}
+	public void JoinEnet()
+	{
+		Menu.Hide();
+		EnetPeer.CreateClient("localhost", PORT);
+		Multiplayer.MultiplayerPeer = EnetPeer;
+	}
+
+	public void AddPlayer(long id)
+	{
+		GetNode("MultiplayerSpawner").Call("_add_player", id);
+	}
+
+	public void RemovePlayer(long id)
+	{
+		GetNode("MultiplayerSpawner").Call("_remove_player", id);
+	}
+
+	public void OnExit()
+	{
+		GetTree().Paused = true;
+		GetTree().ReloadCurrentScene();
+		EnetPeer.Close();
 	}
 }
--- main/multiplayer_spawner.gd
+++ main/multiplayer_spawner.gd
@@ -2,45 +2,31 @@ extends MultiplayerSpawner
 
 var lobby_id : int = 0
 var peer : SteamMultiplayerPeer
-#@export var player_scene : PackedScene
 var is_host : bool = false
 var is_joining : bool = false
-var is_ip : bool = false
 
 @onready var menu : Control = $Menu
 @onready var btn_host : Button = $Menu/Host
 @onready var btn_join : Button = $Menu/Join
 @onready var txt_input : LineEdit = $Menu/Prompt
-@onready var btn_ip : Button = $Menu/Lan
 
-@onready var pause : Control = $Pause
 @onready var btn_exit : Button = $Pause/Exit
 @onready var btn_copy : Button = $Pause/Copy
-@onready var display_id = $Pause/ID
-
-@onready var multiplayer_spawner : MultiplayerSpawner = $"."
-
-const PORT = 9999
-var enet_peer : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+@onready var display_id : Label = $Pause/ID
 
 func manual_ready():
-	multiplayer_spawner.add_spawnable_scene("res://player/Player.tscn")
+	btn_host.pressed.connect(_on_host_pressed)
+	btn_join.pressed.connect(_on_join_pressed)
+	txt_input.text_changed.connect(_on_prompt_text_changed)
+
+	btn_exit.pressed.connect(_on_exit_pressed)
+	btn_copy.pressed.connect(_on_copy_pressed)
+
 	if Steam.isSteamRunning():
 		Steam.steamInitEx(480, true)
 		Steam.initRelayNetworkAccess()
 		Steam.lobby_created.connect(_on_lobby_created)
 		Steam.lobby_joined.connect(_on_lobby_joined)
-	else:
-		_on_lan_pressed()
-
-func _unhandled_input(event : InputEvent) -> void:
-	if event.is_action_pressed("escape") and not menu.is_visible_in_tree():
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-		pause.show()
-	#should move this to resume button
-	if event is InputEventMouseButton and not menu.is_visible_in_tree() and not Input.mouse_mode == Input.MOUSE_MODE_CONFINED_HIDDEN:
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-		pause.hide()
 
 func host_lobby():
 	Steam.createLobby(Steam.LobbyType.LOBBY_TYPE_FRIENDS_ONLY, 16)
@@ -57,6 +43,7 @@ func _on_lobby_created(result : int, lobby_id : int):
 		multiplayer.multiplayer_peer = peer
 		multiplayer.peer_connected.connect(_add_player)
 		multiplayer.peer_disconnected.connect(_remove_player)
+		get_parent().add_child(preload("res://maps/hub/Hub.tscn").instantiate())
 		_add_player()
 		
 		print("Lobby created, join code: ", lobby_id)
@@ -95,10 +82,7 @@ func _remove_player(id : int):
 
 
 func _on_host_pressed() -> void:
-	if is_ip:
-		host_enet()
-	else:
-		host_lobby()
+	host_lobby()
 
 
 func _on_prompt_text_changed(new_text: String) -> void:
@@ -106,47 +90,14 @@ func _on_prompt_text_changed(new_text: String) -> void:
 
 
 func _on_join_pressed() -> void:
-	if is_ip:
-		join_enet()
-	else:
-		join_lobby(txt_input.text.to_int())
+	join_lobby(txt_input.text.to_int())
 
 
 func _on_exit_pressed() -> void:
-	if !is_ip:
-		peer.close()
+	peer.close()
 	Steam.steamShutdown()
 	get_tree().reload_current_scene()
 
 
 func _on_copy_pressed() -> void:
 	DisplayServer.clipboard_set(str(lobby_id))
-
-func _on_lan_pressed() -> void:
-	is_ip = !is_ip
-	if is_ip:
-		btn_ip.text = "Localhost"
-		txt_input.hide()
-		btn_copy.hide()
-		btn_join.disabled = false
-	else:
-		btn_ip.text = "SteamInit"
-		txt_input.show()
-		btn_copy.show()
-		btn_join.disabled = true
-	if !Steam.isSteamRunning() and !is_ip:
-		_on_lan_pressed()
-
-func host_enet() -> void:
-	menu.hide()
-	enet_peer.create_server(PORT)
-	multiplayer.multiplayer_peer = enet_peer
-	multiplayer.peer_connected.connect(_add_player)
-	multiplayer.peer_disconnected.connect(_remove_player)
-
-	_add_player(multiplayer.get_unique_id())
-
-func join_enet():
-	menu.hide()
-	enet_peer.create_client("localhost", PORT)
-	multiplayer.multiplayer_peer = enet_peer

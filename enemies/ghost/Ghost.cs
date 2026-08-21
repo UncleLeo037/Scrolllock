f435057844506bf53f880561844435f3b0d25215
UncleLeo037 <leo.muller@gmail.com>
2026-08-21 19:35:50 +0100

    enemy adjustments

--- enemies/ghost/Ghost.cs
+++ enemies/ghost/Ghost.cs
@@ -3,22 +3,27 @@ using System;
 
 public partial class Ghost : CharacterBody3D
 {
-	private NavigationAgent3D navAgent;
+	private NavigationAgent3D _navAgent;
+	private Player _target;
+	public float speed = 3.0f;
+	public float memory = 15.0f;
 
-
-
-	private float speed = 3.0f;
-	private Player target;
-	private float memory = 10.0f;
+	// public Ghost(float speed, float size, float memory)
+	// {
+	// 	this.speed = speed;
+	// 	Scale = Scale*size;
+	// 	this.memory = memory;
+	// }
 
 	public override void _Ready()
 	{
-		navAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
-		navAgent.PathDesiredDistance = 0.5f;
-		navAgent.TargetDesiredDistance = 0.5f;
+		_navAgent = GetNode<NavigationAgent3D>("NavigationAgent3D");
+		_navAgent.PathDesiredDistance = 0.5f;
+		_navAgent.TargetDesiredDistance = 0.5f;
 
 		Area3D vision = GetNode<Area3D>("Area3D");
 		vision.BodyEntered += Detect;
+		vision.BodyExited += Hidden;
 	}
 
 	public override void _PhysicsProcess(double delta)
@@ -30,23 +35,18 @@ public partial class Ghost : CharacterBody3D
 		}
 		else
 		{
-			if (target != null)
+			if (_target != null)
 			{
-				navAgent.TargetPosition = target.GlobalTransform.Origin;
-				memory -= 1 * (float)delta;
-				if (memory < 0)
-				{
-					target = null;
-					memory = 5.0f;
-				}
+				_navAgent.TargetPosition = _target.GlobalTransform.Origin;
 			}
-			if (navAgent.IsNavigationFinished())
+			if (_navAgent.IsNavigationFinished())
 			{
+				Attack();
 				return;
 			}
 
 			Vector3 currentAgentPosition = GlobalTransform.Origin;
-			Vector3 nextPathPosition = navAgent.GetNextPathPosition();
+			Vector3 nextPathPosition = _navAgent.GetNextPathPosition();
 
 			Velocity = currentAgentPosition.DirectionTo(nextPathPosition) * speed;
 
@@ -57,11 +57,41 @@ public partial class Ghost : CharacterBody3D
 		MoveAndSlide();
 	}
 
+	public override void _Process(double delta)
+	{
+		memory -= 1 * (float)delta;
+		if (memory < 0)
+		{
+			_target = null;
+			SetProcess(false);
+		}
+	}
+
+
+	public void Attack()
+	{
+		if (_target != null)
+		{
+			//check that _target Player is in attack area and then deal damage
+			//also add some delay between attacks and before can move again
+		}
+	}
+
 	public void Detect(Node3D node)
 	{
 		if (node is Player player)
 		{
-			target = player;
+			_target = player;
+			SetProcess(false);
+		}
+	}
+
+	public void Hidden(Node3D node)
+	{
+		if (node is Player)
+		{		
+			memory = 5.0f;
+			SetProcess(true);
 		}
 	}
 }
